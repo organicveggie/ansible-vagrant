d965de3417091828355620121a9ebe250f3e7269
Sean Laurent <organicveggie@gmail.com>
2022-10-20 18:26:56 -0700

    Initial commit
    Fully functional. Includes Molecule tests and GitHub automation.

---
+++ setup-virtualenv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo virtualenv venv
+virtualenv venv
+. venv/bin/activate
+
+echo pip install --upgrade pip
+pip install --upgrade pip
+
+echo pip install ansible molecule "ansible-lint" "molecule[docker]"
+pip install ansible molecule "ansible-lint" "molecule[docker]"
+
+echo Remember to run "source venv/bin/activate"
